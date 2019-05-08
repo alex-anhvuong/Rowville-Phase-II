21e4b0c32ba920f68dbc56b13db917a37900fa6e
Mathew Norman <spam@matnorman.com>
2019-05-09 08:30:14 +1000

    added a simple create table script.

---
+++ php/extra/create.tables.php
@@ -0,0 +1,43 @@
+<?php
+
+
+    // Database connection parameters
+    $dbhost = "localhost";
+    $dbname = "thinkbiz";
+    $dbuser = "username";
+    $dbpass = "password";
+
+    // Create a new PDO connection
+    $pdo = new PDO("mysql:host=$dbhost;dbname=$dbname;$dbuser;$dbpass");
+
+    $sql = "CREATE TABLE IF NOT EXISTS users (
+        id INT(9) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+        firstname VARCHAR(30) NOT NULL,
+        lastname VARCHAR(30) NOT NULL,
+        email VARCHAR(50) NOT NULL UNIQUE,
+        passhash VARCHAR(128) NOT NULL
+    ); CREATE TABLE IF NOT EXISTS analytics (
+        id INT(20) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+        dtstamp TIMESTAMP NOT NULL,
+        pageaccessed VARCHAR(100) NOT NULL,
+        devicetype VARCHAR(50) NOT NULL,
+        ipaddr VARCHAR(16) NOT NULL
+    ); CREATE TABLE IF NOT EXISTS userprog (
+        id INT(9) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+        user_id INT(9) NOT NULL,
+        step_complete INT(6) NOT NULL
+    ); CREATE TABLE IF NOT EXISTS content (
+        id INT(9) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+        page_id VARCHAR(30) NOT NULL,
+        content_html LONGTEXT NOT NULL
+    )";
+        
+    try {
+        $pdo->exec($sql);
+    } catch (Exception $e) {
+        throw new Exception($e);
+    }
+
+    $pdo = null;
+
+?>
